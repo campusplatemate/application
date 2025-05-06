fce72d5694bc7f05ea6a22fec9f086983f6a0c05
jaylin-m <jaylin.m3921@gmail.com>
2025-05-05 18:31:47 -1000

    issue-33 completed

--- src/app/rewards/page.tsx
+++ src/app/rewards/page.tsx
@@ -1,10 +1,9 @@
 import { getServerSession } from 'next-auth';
 import { loggedInProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
-import { Col, Container, Row } from 'react-bootstrap';
 import { RewardItem } from '@prisma/client';
 import { prisma } from '@/lib/prisma';
-import RewardCard from '@/components/RewardCard';
+import RewardsClient from '@/components/RewardsClient';
 
 const RewardsPage = async () => {
   const session = await getServerSession(authOptions);
@@ -18,16 +17,7 @@ const RewardsPage = async () => {
 
   return (
     <main>
-      <Container fluid className="py-3 px-3">
-        <h1 className="text-center mb-4">Rewards</h1>
-        <Row xs={1} md={2} lg={3} className="g-4">
-          {rewards.map((item) => (
-            <Col key={item.id}>
-              <RewardCard item={item} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      <RewardsClient rewards={rewards} />
     </main>
   );
 };
